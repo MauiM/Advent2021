955aae7609c1019ab878ccd9b3e18776d5755446
Maui McAdams <mmcadams@savers.com>
2021-12-03 08:03:11 -0800

    Day 3

--- Advent2021/Days/Day03.cs
+++ Advent2021/Days/Day03.cs
@@ -5,31 +5,83 @@ namespace Advent2021.Days
 {
     internal class Day03
     {
-        private class Data
-        {
-            public int id { get; set; }
-        }
-
         public Day03() { }
 
         public string Part1()
         {
-            int valid = 0;
+            var lines = Data03.Puzzle.ToStringList();
 
-            var lines = Data02.Puzzle.ToObjectList<Data>(seperator: "", keyValueSeperator: " ");
+            var size = lines[0].Length;
+            var gamma = new int[size];
+            var epsilon = new int[size];
+
             foreach (var line in lines)
             {
-                
+                for (int i = 0; i < size; i++)
+                {
+                    gamma[i] += (line[i] == '1' ? 1 : -1);
+                }
             }
 
-            return $"{valid}";
+            for (int i = 0; i < size; i++)
+            {
+                gamma[i] = gamma[i] > 0 ? 1 : 0;
+                epsilon[i] = gamma[i] > 0 ? 0 : 1;
+            }
+
+            int iGamma = Convert.ToInt32(string.Join("", gamma), 2);
+            int iEpsilon = Convert.ToInt32(string.Join("", epsilon), 2);
+
+            return $"{iGamma * iEpsilon}";
         }
 
         public string Part2()
         {
-            int valid = 0;
+            var lines = Data03.Puzzle.ToStringList();
 
-            return $"{valid}";
+            var size = lines[0].Length;
+            var gamma = new int[size];
+            var epsilon = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                int ones = lines.Count(x => x[i] == '1');
+                int zeros = lines.Count(x => x[i] == '0');
+
+                if (lines.Count > 1)
+                {
+                    gamma[i] = ones >= zeros ? 1 : 0;
+                }
+                else
+                {
+                    gamma[i] = lines[0][i] == '1' ? 1 : 0;
+                }
+
+                lines = lines.Where(x => x[i].ToString() == gamma[i].ToString()).ToList();
+            }
+
+            lines = Data03.Puzzle.ToStringList();
+            for (int i = 0; i < size; i++)
+            {
+                int ones = lines.Count(x => x[i] == '1');
+                int zeros = lines.Count(x => x[i] == '0');
+
+                if (lines.Count > 1)
+                {
+                    epsilon[i] = ones >= zeros ? 0 : 1;
+                }
+                else
+                {
+                    epsilon[i] = lines[0][i] == '1' ? 1 : 0;
+                }
+
+                lines = lines.Where(x => x[i].ToString() == epsilon[i].ToString()).ToList();
+            }
+
+            int iGamma = Convert.ToInt32(string.Join("", gamma), 2);
+            int iEpsilon = Convert.ToInt32(string.Join("", epsilon), 2);
+
+            return $"{iGamma * iEpsilon}";
         }
     }
 }
